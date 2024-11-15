f1315d632889c848fe634260d783a69e32624085
SaahilTamboli <saahil.tamboli1@gmail.com>
2024-11-15 23:53:54 +0530

    deploy updated

--- app/main.py
+++ app/main.py
@@ -31,7 +31,11 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+    allow_origins=[
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+        "https://verity-bggwhqaqeze3agd2.westindia-01.azurewebsites.net"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- startup.sh
+++ startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/site/wwwroot/app
-source ../antenv/bin/activate
+cd /home/site/wwwroot
+source antenv/bin/activate
+cd app
+export ALLOWED_HOSTS="verity-bggwhqaqeze3agd2.westindia-01.azurewebsites.net"
 uvicorn main:app --host 0.0.0.0 --port 8000
